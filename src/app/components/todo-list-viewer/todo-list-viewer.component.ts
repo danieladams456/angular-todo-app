0654f5b769eb6154308919ecc70094af4bf5c5f3
Daniel Adams <danieladams456@gmail.com>
2017-08-30 21:31:41 -0400

    added notes.txt and removed files I won't be using

--- src/app/components/todo-list-viewer/todo-list-viewer.component.ts
+++ src/app/components/todo-list-viewer/todo-list-viewer.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list-viewer',
-  templateUrl: './todo-list-viewer.component.html',
-  styleUrls: ['./todo-list-viewer.component.css']
+  templateUrl: './todo-list-viewer.component.html'
+  // styleUrls: ['./todo-list-viewer.component.css']
 })
 export class TodoListViewerComponent implements OnInit {
 
